96383256cdbdf3febd2df3384963c4b1a7eb6f40
H0uston <HoustonEU@gmail.com>
2022-10-01 19:50:21 +0300

    add support of ts and css modules

---
+++ src/friend.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import styles from './styles.css';
+
+export const FriendText = () => (<span className={styles['friend-text']}>friend</span>)
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(<h1>Hey you</h1>, document.getElementById('root'));
